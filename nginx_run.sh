d9c7e2ac34343d90caf946d92c65ea5d254562ff
linuxvfast <2559811312@qq.com>
2019-09-06 10:26:34 +0800

    emptydir

--- nginx_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/usr/sbin/sshd &
-/usr/sbin/nginx
--- tomcat_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -f /script/.tomcat_admin_created ];then
-    /script/create_tomcat_admin_user.sh
-fi
-
-/usr/sbin/sshd -D &
-exec ${CATALINA_HOME}/bin/catalina.sh run
